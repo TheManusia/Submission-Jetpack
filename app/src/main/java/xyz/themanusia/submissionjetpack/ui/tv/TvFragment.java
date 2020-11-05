739430041aa37a62c64701f09d113a24d0ca81a6
Ian <minecraftindo0@gmail.com>
2020-11-05 20:52:12 +0700

    Add Tv ViewModel

--- app/src/main/java/xyz/themanusia/submissionjetpack/ui/tv/TvFragment.java
+++ app/src/main/java/xyz/themanusia/submissionjetpack/ui/tv/TvFragment.java
@@ -1,17 +1,17 @@
 package xyz.themanusia.submissionjetpack.ui.tv;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
@@ -20,10 +20,11 @@ import butterknife.ButterKnife;
 import lombok.NoArgsConstructor;
 import xyz.themanusia.submissionjetpack.R;
 import xyz.themanusia.submissionjetpack.data.TvEntity;
-import xyz.themanusia.submissionjetpack.utils.DataDummy;
 
 @NoArgsConstructor
 public class TvFragment extends Fragment {
+    private TvViewModel viewModel;
+
     @BindView(R.id.rvTv)
     RecyclerView rvTv;
     @BindView(R.id.pbTv)
@@ -45,7 +46,8 @@ public class TvFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         if (getActivity() != null) {
-            List<TvEntity> tvEntities = DataDummy.generateTvData();
+            viewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(TvViewModel.class);
+            List<TvEntity> tvEntities = viewModel.getTvList();
             pbTv.setVisibility(View.GONE);
 
             rvTv.setLayoutManager(new LinearLayoutManager(getContext()));
